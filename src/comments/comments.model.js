b0efc873d16d0ad42d0c6c00404efd2b2a017a51
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-02-29 21:04:56 -0600

    Model from Comments added

---
+++ src/comments/comments.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+const CommentSchema = mongoose.Schema({
+    publicationId:{
+        type: Schema.Types.ObjectId,
+        ref: 'Publication'
+    },
+    commentTitle:{
+        type: String,
+        require: [true, "Required field"]
+    },
+    commentContent:{
+        type: String,
+        require: [true, "Required field"]
+    },
+    status:{
+        type: Boolean,
+        default:true
+    }
+})
+
+CommentSchema.methods.toJSON=function(){
+    const { __v, _id, status, ...comment} = this.toObject();
+    comment.uid = _id;
+    return comment
+}
+
+export default mongoose.model('Comment', CommentSchema);
